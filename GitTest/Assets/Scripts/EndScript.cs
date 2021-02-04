a8e85a3b440d139027d937550f9b903ad108c731
Wi024933 <beattywf@knights.ucf.edu>
2021-02-04 13:29:56 -0500

    Added some UI, endscene, stuff like that

---
+++ GitTest/Assets/Scripts/EndScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class EndScript : MonoBehaviour
+{
+
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
---
+++ GitTest/Assets/Scripts/ScoreScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class ScoreScript : MonoBehaviour
+{
+    public static ScoreScript instance;
+    public TextMeshProUGUI text;
+
+    int score;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    public void ChangeScore(int banana)
+    {
+        score += banana;
+        text.text = "x" + score.ToString();
+    }
+}
