a479ef3d813910b2a24eb760b789e03cc097bc6e
Wi024933 <beattywf@knights.ucf.edu>
2021-02-07 13:29:44 -0500

    Getting CHarlie's Stuff
    UI updated, Collectibles Added, Beaver Done

---
+++ GitTest/Assets/LetterScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LetterScript : MonoBehaviour
+{
+    public int letters = 1;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            ScoreScript.instance.ChangeLetters(letters);
+        }
+    }
+}
