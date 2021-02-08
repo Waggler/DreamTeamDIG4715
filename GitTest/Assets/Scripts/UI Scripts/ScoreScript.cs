67d7cd61ae6c437efec35278eed68d1b3c4724fa
Wi024933 <beattywf@knights.ucf.edu>
2021-02-08 13:33:04 -0500

    KONG
    Letters adjusted, moved scripts into folders

---
+++ GitTest/Assets/Scripts/UI Scripts/ScoreScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class ScoreScript : MonoBehaviour
+{
+    public static ScoreScript instance;
+    public TextMeshProUGUI bananaText;
+    public TextMeshProUGUI livesText;
+
+    int score;
+    int lives;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    public void ChangeScore(int banana)
+    {
+        score += banana;
+        bananaText.text = "x" + score.ToString();
+    }
+
+    public void ChangeLives(int balloon)
+    {
+        lives += balloon;
+        livesText.text = "x" + lives.ToString();
+    }
+}
